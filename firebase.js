c934d8d6b375cb5ba82fcb599d313e9f6c85ef81
Anurag Mishra <97835032+shaktimaan00@users.noreply.github.com>
2022-11-03 21:04:53 +0530

    upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBo0y52gP6kc1krN3tXDYC-2NJLhFzJvyM",
+  authDomain: "fir-authentication-79f9b.firebaseapp.com",
+  databaseURL:
+    "https://fir-authentication-79f9b-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "fir-authentication-79f9b",
+  storageBucket: "fir-authentication-79f9b.appspot.com",
+  messagingSenderId: "349450255143",
+  appId: "1:349450255143:web:77d96fdb1552d4d46a8702",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
